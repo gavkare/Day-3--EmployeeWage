41c96b521d484ec54e9ea11f46d00de660be51f6
Shankar Gavkare <shankargavkare2277@gmail.com>
2023-01-19 17:20:05 +0530

    UC5

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,28 +1,29 @@
-package Employee;
+package Employeeg;
 import java.util.Random;
-public class EmployeeWage {
+public class EmployeeWage{
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
 
-        Random attendance = new Random();
+        Random attendance =new Random();
         int atndc = attendance.nextInt(2);
-        if (atndc == 0) {
+        if(atndc ==0) {
             System.out.println("employee is absent");
-        } else {
+        }
+        else {
             System.out.println("employee is present");
         }
-        int wagePerHour = 20;
+        int wagePerHour =20;
         int fullDay = 8;
-        int dailyWage = (wagePerHour * fullDay);
+        int dailyWage = (wagePerHour*fullDay);
         System.out.println("Daily employee Wage is  " + dailyWage);
 
 
         int partTimeWage = 10;
         int partTimeHour = 8;
-        int partimeWage = (partTimeWage * partTimeHour);
+        int partimeWage = (partTimeWage*partTimeHour);
         System.out.println("Part time wage is  " + partimeWage);
 
-        switch (atndc) {
+        switch(atndc) {
             case 0:
                 System.out.println("Absent");
                 break;
@@ -30,4 +31,22 @@ public class EmployeeWage {
                 System.out.println("Present");
                 break;
         }
+
+        int workingDayMonth = 20;
+        int wageInMonth = (workingDayMonth*dailyWage);
+        System.out.println("Wage in a month is  " + wageInMonth);
+
+        int workingHours=0;
+        int workingDay =0;
+        int wage;
+        while(workingHours<=100||workingDay<=20) {
+            wage =workingHours*workingDay;
+            workingHours++;
+            workingDay++;
+
+        }
+
     }
+
+
+}
